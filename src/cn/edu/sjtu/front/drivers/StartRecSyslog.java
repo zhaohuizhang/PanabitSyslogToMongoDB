f589215b9bf61ffade9f660c7ab765f94e964b1c
Jianwen Wei <wei.jianwen@gmail.com>
2011-07-24 21:06:16 +0800

    Adjust the debug output code.

--- src/cn/edu/sjtu/front/drivers/StartRecSyslog.java
+++ src/cn/edu/sjtu/front/drivers/StartRecSyslog.java
@@ -26,33 +26,35 @@ import cn.edu.sjtu.front.panabitsyslog.PanabitMsgParserApp;
 
 /**
  * @author jianwen,zhangzhaohui
- *
+ * 
  */
 public class StartRecSyslog {
 
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-        //create new file to store the udp packet		
-//		File test=new File("syslog.txt");
-//		if(test.exists()){
-//		    System.out.print("this file does not exist");
-//		   }else{
-//		    System.out.print("this file  exist");
-//		    test.createNewFile();//create new file
-//		   }
-		
-	    SimpleDateFormat df=new SimpleDateFormat("yyyyMMdd");
+		// create new file to store the udp packet
+		// File test=new File("syslog.txt");
+		// if(test.exists()){
+		// System.out.print("this file does not exist");
+		// }else{
+		// System.out.print("this file  exist");
+		// test.createNewFile();//create new file
+		// }
+
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
 		String strMsg = null;
 		PanabitMsg msgPanabit = null;
 		PanabitMsgParser panabitParser = new PanabitMsgParser();
-        Mongo mongo = new Mongo("10.50.15.206");
-        DB db = mongo.getDB("DBsyslog");
-        DBCollection panabitsyslogs = db.getCollection("panabit_"+df.format(new Date()));
-//        DBCollection panabitCollection = db.getCollection("panabit_20110706");
-//        System.out.println(panabitCollection.find().count());
+		Mongo mongo = new Mongo("10.50.15.206");
+		DB db = mongo.getDB("DBsyslog");
+		DBCollection panabitsyslogs = db.getCollection("panabit_"
+				+ df.format(new Date()));
+		// DBCollection panabitCollection =
+		// db.getCollection("panabit_20110706");
+		// System.out.println(panabitCollection.find().count());
 		try {
 			DatagramSocket receiveScoket = new DatagramSocket(30514);
 			byte buf[] = new byte[1024];
@@ -60,41 +62,37 @@ public class StartRecSyslog {
 			System.out.println("Start to Receive Syslog Packet.");
 			// TOOD: Remove the file writing statements
 			// BufferedWriter output = new BufferedWriter(new FileWriter(test));
-			while(true){
+			while (true) {
 				receiveScoket.receive(receivePacket);
-				char rawMsg[] = new String(receivePacket.getData()).toCharArray();
-				// TOOD: Remove the console output the RAW UDP Message (for Debug Only)
+				char rawMsg[] = new String(receivePacket.getData())
+						.toCharArray();
+				// TOOD: Remove the console output the RAW UDP Message (for
+				// Debug Only)
 				// System.out.println(rawMsg);
 				int udpSize = receivePacket.getLength();
 				String strUDP = new String(rawMsg, 0, udpSize);
 				// System.out.println(strUDP);
 				msgPanabit = panabitParser.parseMsg(strUDP);
-				
-				
-				// System.out.println(msgPanabit);
-				
-				if (msgPanabit == null)
-					System.out.println("Null Msg:" + strUDP);
-				
-				if(msgPanabit != null){
-				// System.out.println(msgPanabit.toMongoDBObj());
-				panabitsyslogs.insert(msgPanabit.toMongoDBObj());
-				}			
 
-			    
+				if (msgPanabit != null) {
+					System.out.println(msgPanabit);
+					// System.out.println(msgPanabit.toMongoDBObj());
+					panabitsyslogs.insert(msgPanabit.toMongoDBObj());
+				}
+
 			}
-				
+
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		// LOOP
 		// TODO: Read a Message(String) from UDP
 		// TODO: Parse the Message
-//		msgPanabit = parser.parseMsg(strMsg);
+		// msgPanabit = parser.parseMsg(strMsg);
 		// TODO
-		// LOOP END 
+		// LOOP END
 	}
 
 }
