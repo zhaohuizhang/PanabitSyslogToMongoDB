9abf441196da48556b9f9dc767f5528861fa5bcd
Jianwen Wei <wei.jianwen@gmail.com>
2011-08-18 20:41:44 +0800

    Remove the starting script(run.sh, run.bat) in bin/ -- use the ones in project's root  dir.

--- bin/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -classpath ./bin;./lib/mongo-2.6.3.jar cn.edu.sjtu.front.drivers.StartRecSyslog 10.50.7.190 27017
-pause
--- bin/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-java -classpath ./bin:./lib/mongo-2.6.3.jar cn.edu.sjtu.front.drivers.StartRecSyslog 10.50.15.201 27017
-
